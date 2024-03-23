a625cd45800c13dd5e68b6ebb3ea1c1a3bb5b374
darthpedroo <pedrovillarino06@gmail.com>
2024-03-23 12:12:35 -0300

    cambios troll

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from aiohttp import web
 import socketio
+import tracemalloc
 
 sio = socketio.AsyncServer(cors_allowed_origins="*",)
 app = web.Application()
 sio.attach(app)
+tracemalloc.start()
+
 
 @sio.event
 def connect(sid, environ):
@@ -17,6 +20,11 @@ async def chat_message(sid, data):
 def disconnect(sid):
     print('disconnect ', sid)
 
+@sio.event
+
+def repartir_cartas(sid):
+    print("anda")
+    sio.emit('CartasJugador', [1,2,3])
 
 if __name__ == '__main__':
     web.run_app(app)
