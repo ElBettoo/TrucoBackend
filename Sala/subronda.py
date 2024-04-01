f0d1e2b4c3246688df20ca6db404e46efd8f8900
darthpedroo <pedrovillarino06@gmail.com>
2024-04-01 03:43:49 -0300

    agregué la subronda

---
+++ Sala/subronda.py
@@ -0,0 +1,46 @@
+class SubRonda:
+    def __init__(self, num_players) -> None:
+        self.cartas_jugadas_subronda = [] # esto se tiene que ver asi [{porky: 2 espada}, {joaquin caca: 10 de oro}, ]
+        self.num_players = num_players
+        
+
+    def append_to_cartas_jugadas_subronda(self, carta_jugada):
+        self.cartas_jugadas_subronda.append(carta_jugada)
+        #self.trigger_win_checker()
+
+    def restore_cartas_jugadas_subronda(self):
+        self.cartas_jugadas_subronda = []
+
+    def get_numPlayers(self):
+        return self.num_players
+    
+    def get_cartas_jugadas_subronda(self):
+        return self.cartas_jugadas_subronda
+
+
+    def trigger_win_checker(self): #este metodo se puede borrar pqe se triguera cuando se guardaron las mismas cartas que personas pero la verdad es medio xD pq se puede hacer lo mismo pero despeus V: 
+        if len(self.cartas_jugadas_subronda) == self.num_players:
+            return self.get_winner_sub_round()
+    
+    def get_winner_sub_round(self):
+
+        win_card = ""
+        win_player = ""
+        empate = []
+
+
+        for par in self.cartas_jugadas_subronda: #par es una tupla que contiene el usuario y la carta que jugó. Con un dictionario era super explosive asi qeu mejro no lo hagamos con dict
+            
+            player = par[0]
+            card = par[1]
+
+            print("carta de  :" , player ," : ", card)
+            
+            if card > win_card:
+                win_card = card
+                win_player = player
+                empate = []
+            elif card == win_card:
+                empate.append(win_card, card, win_player, player)
+
+        return win_player, win_card  if len(empate) == 0 else empate
