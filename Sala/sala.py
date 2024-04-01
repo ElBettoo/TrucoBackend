428160dfd736f59c8d9e3ad1ce5b0e66a0ee2a5c
darthpedroo <pedrovillarino06@gmail.com>
2024-04-01 03:46:26 -0300

    Implemente la logica de los equipos en la sala

--- Sala/sala.py
+++ Sala/sala.py
@@ -1,11 +1,38 @@
 from .ronda import Ronda
+from .equipo import Equipo
+
 
 class Sala:
     def __init__(self, codigo_sala, cantidad_jugadores, has_flor, points_to_win, jugadores_en_sala) -> None:
 
         self.codigo_sala = codigo_sala
-        self.ronda = Ronda()
+        
+        self.equipos = [Equipo("Los chinchulines "), Equipo("Skibidi sigma ")]
+
+        self.ronda = Ronda(cantidad_jugadores)
         self.cantidad_jugadores = cantidad_jugadores
         self.has_flor = has_flor
         self.points_to_win = points_to_win
         self.jugadores_en_sala = jugadores_en_sala
+
+    def get_ronda(self):
+        return self.ronda
+
+    def recibir_cartas(self, carta):
+        self.get_ronda().get_sub_ronda().append_to_cartas_jugadas_subronda(carta)
+
+    def agregar_jugador_equipo(self, jugador, num_equipo):
+        jugador.perform_set_num_equipo(num_equipo)
+        self.equipos[num_equipo].add_jugador(jugador)
+
+    def mostrar_jugadores_equipo(self, num_equipo):
+        return self.equipos[num_equipo].get_jugadores()
+    
+    def get_equipo(self, num_equipo):
+        return self.equipos[num_equipo]
+
+
+
+
+
+    
