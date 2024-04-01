fef2e3699c1a327129d1e594ed984977699f76dd
bettoo <joaco_fer@yahoo.com.ar>
2024-04-01 18:02:18 -0300

    feat: change getters into properties and added more functions

--- Sala/equipo.py
+++ Sala/equipo.py
@@ -1,30 +1,45 @@
 class Equipo:
     def __init__(self, nombre) -> None:
-        self.nombre =  nombre
-        self.jugadores = []
-        self.puntos_ronda = 0
-        self.puntos_partida = 0
+        self._nombre =  nombre
+        self._players = []
+        self._puntos_subronda = 0
+        self._puntos_partida = 0
+        self._puntos_ronda = 0
 
     def add_jugador(self, jugador):
-        self.jugadores.append(jugador)
-
-    def get_jugadores(self):
-        return self.jugadores
+        self._players.append(jugador)
     
-    def sumar_punto_ronda(self):
-        self.puntos_ronda +=1 
+    def add_puntos_subronda(self):
+        self._puntos_subronda += 1 
+
+    def add_puntos_ronda(self, cantidad_puntos):
+        self._puntos_ronda += cantidad_puntos
     
-    def sumar_puntos_partida(self):
-        self.puntos_partida += 1
+    def reset_puntos_ronda(self):
+        self._puntos_partida += self._puntos_ronda
+        self._puntos_ronda = 0
+        self._puntos_subronda = 0
 
-    def get_puntos_ronda(self):
-        return self.puntos_ronda
-
-    def restart_puntos_ronda(self):
-        self.puntos_ronda = 0
-
-    def get_puntos_partida(self):
-        return self.puntos_partida
+    @property
+    def players(self):
+        return self._players
     
-    def get_nombre(self):
-        return self.nombre
+    @property
+    def puntos_subronda(self):
+        return self._puntos_subronda
+
+    @property
+    def puntos_ronda(self):
+        return self._puntos_ronda
+    
+    @property
+    def puntos_partida(self):
+        return self._puntos_partida
+    
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, otro_nombre):
+        self._nombre = otro_nombre
