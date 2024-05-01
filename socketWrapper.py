f6031b85de52daf357cf7c0d2fb2af67cfb3c6ad
darthpedroo <pedrovillarino06@gmail.com>
2024-05-01 12:41:16 -0300

    Cards are sent

--- socketWrapper.py
+++ socketWrapper.py
@@ -2,6 +2,8 @@ import asyncio
 import aiohttp.web
 import socketio
 import tracemalloc
+import random
+
 
 class SocketIOApp:
     def __init__(self):
@@ -12,6 +14,7 @@ class SocketIOApp:
         self.sio.on('connect', self.on_connect)
         self.sio.on('disconnect', self.on_disconnect)
         self.sio.on('repartir_cartas', self.repartir_cartas)
+        self.sio.on('on_join_room', self.on_join_room)
 
     async def on_connect(self, sid, environ):
         print("El socket:", sid, 'se conectó')
@@ -19,10 +22,19 @@ class SocketIOApp:
     async def on_disconnect(self, sid):
         print('Desconexión de', sid)
 
-    async def repartir_cartas(self, sid):
-        print("Manejando repartición de cartas!")
-        await self.sio.emit('repartir_cartas', ['011'], to=sid)
+    async def repartir_cartas(self, sid, salaId):
+        print("Manejando repartición de cartas!: ", sid)
+        print("SID: ", sid)
+        print("SalaID: ", salaId)
+        await self.sio.emit('repartir_cartas', ['1', 'random.choice(100)', 'random.choice(1312412)'], to=salaId)
+
     
+    async def on_join_room(self,sid,SalaId):
+        print("Unido :3: " )
+        print("S_room: ", sid)
+        print("sala_room: ", SalaId)
+        await self.sio.enter_room(sid, SalaId)
+        await self.sio.emit("joined_room")
 
     async def run(self):
         tracemalloc.start()
@@ -31,7 +43,10 @@ class SocketIOApp:
         site = aiohttp.web.TCPSite(runner, 'localhost', 8080)
         await site.start()
 
+    
+
 if __name__ == '__main__':
     socketio_app = SocketIOApp()
     asyncio.get_event_loop().run_until_complete(socketio_app.run())
+    print("[SERVER] ON")
     asyncio.get_event_loop().run_forever()
