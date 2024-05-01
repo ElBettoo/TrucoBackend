59c424e3107b89b9c725cf9d707b6f649c61648d
temp <temp@temp.com>
2024-04-30 22:14:37 -0300

    if this works, no one judges...

--- server.py
+++ server.py
@@ -1,26 +1,23 @@
 from aiohttp import web
 import socketio
-import tracemalloc
-import random
 
-
-sio = socketio.AsyncServer(cors_allowed_origins="*",)
+sio = socketio.AsyncServer(cors_allowed_origins="*")
 app = web.Application()
 sio.attach(app)
-
-tracemalloc.start()
 
 
 @sio.event
 def connect(sid, environ):
-    print("el socket:  ", sid, 'se conecto')
+    print("connect ", sid)
+
+@sio.event
+async def chat_message(sid, data):
+    print("message ", data)
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
+
 if __name__ == '__main__':
     web.run_app(app)
-
-
-
--- socketWrapper.py
+++ socketWrapper.py
@@ -3,24 +3,26 @@ import socketio
 import tracemalloc
 
 class SocketWrapper():
-    
     sio = socketio.AsyncServer(cors_allowed_origins="*")
     app = web.Application()
+
     
     def __init__(self):
         self.initialize_server()
        
     def get_app(self):
         return self.app
-    
+
     def get_sio(self):
         return self.sio
 
 
     # NECESESARIOOO
     def initialize_server(self):
+        self.sio.attach(app)
+        tracemalloc.start()
+
         
-    
         @self.sio.event
         def connect(socket_id, _):
             print('connected', socket_id)
@@ -29,14 +31,14 @@ class SocketWrapper():
         def disconnect(sid):
             print('disconnected ', sid)
 
-        print('[SERVER] INITIALIZED')
+        @self.sio.event
+        def repartir_cartas(sid):  
+            self.sio.emit(('repartir_cartas', ['011', '012', '123']))          
             
 
 
 objetoSocket = SocketWrapper()
 app = objetoSocket.get_app()
-sio = objetoSocket.get_sio()
-sio.attach(app)
 
 if __name__ == '__main__':
     web.run_app(app)
