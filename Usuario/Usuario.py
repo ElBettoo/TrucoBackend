435f6762d0187a161bcc488a0daccb5079e2e7f0
darthpedroo <pedrovillarino06@gmail.com>
2024-05-09 23:44:58 -0300

    Getters del usuario

--- Usuario/Usuario.py
+++ Usuario/Usuario.py
@@ -6,3 +6,14 @@ class Usuario:
         
     def set_mano(self, nueva_mano):
         self.mano = nueva_mano
+
+    def get_mano(self):
+        mano = self.mano.cartas
+        mano_serialized = []
+        for carta in mano:
+            mano_serialized.append(carta.key)
+        return mano_serialized
+    
+    
+    def get_socket_id(self):
+        return self.socketId
